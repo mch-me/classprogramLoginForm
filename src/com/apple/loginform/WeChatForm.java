7f52373afd05663e16cad816a7e01e768adfdacc
mch <827313519@qq.com>
2019-11-11 17:43:21 +0800

    add LoginForm

---
+++ src/com/apple/loginform/WeChatForm.java
@@ -0,0 +1,9 @@
+package com.apple.loginform;
+
+public class WeChatForm {
+    public static void main(String[] args) {
+        LoginForm loginForm = new LoginForm();
+
+        loginForm.setFrameLoginVisible(true);
+    }
+}
